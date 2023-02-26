b87825df1c41539c327cd0152baeea313585866c
n67h <night67hunter@gmail.com>
2023-02-26 11:57:20 +0800

    added modal for each date

--- app/calendar.php
+++ app/calendar.php
@@ -89,7 +89,30 @@
             if($date < date('Y-m-d')){
                 $calendar.="<td><h4>$currentDay</h4> <button class='btn btn-danger btn-xs'>N/A</button>";
             }else{
-                $calendar.="<td class='$today'><h4>$currentDay</h4> <a href='book.php?date=".$date."' class='btn btn-success btn-xs'>Book</a>";
+                // $calendar.="<td class='$today'><h4>$currentDay</h4> <a href='book.php?date=".$date."' class='btn btn-success btn-xs'>Book</a>";
+
+                // Button trigger modal
+                $calendar.="<td class='$today'><h4>$currentDay</h4> <button type='button' class='btn btn-success btn-xs' data-bs-toggle='modal' data-bs-target='#date-".$date."'>Book</button>";
+            ?>
+                <!-- Modal -->
+                <div class="modal fade" id="date-<?= $date; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel"><?= $date; ?></h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                ...
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php  
             }
                 
             $calendar .="</td>";
