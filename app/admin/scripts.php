6125297acf51b7b6a9152c677cd673765a10bd6d
n67h <night67hunter@gmail.com>
2023-05-09 04:39:33 +0800

    Add scripts for charts

--- app/admin/scripts.php
+++ app/admin/scripts.php
@@ -25,6 +25,84 @@
             $('.sidebar').removeClass('active');
         });
     </script>
+    <script>
+        // const labels = Utils.months({count: 7});
+        const labels = <?php echo json_encode($month); ?>;
+        const data = {
+        labels: labels,
+        datasets: [{
+            label: 'Total appointments per month',
+            data: <?php echo json_encode($appointment); ?>,
+            backgroundColor: [
+            'rgba(255, 99, 132, 0.7)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
+            ],
+            borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+            ],
+            borderWidth: 4
+        }]
+        };
+
+        const config = {
+        type: 'bar',
+        data: data,
+        options: {
+            scales: {
+            y: {
+                beginAtZero: true
+            }
+            }
+        },
+        };
+
+        const myChart = new Chart(
+            document.getElementById('myChart1'),
+            config
+        );
+    </script>
+    <script>
+    const labels2 = <?php echo json_encode($category); ?>;
+    const data2 = {
+    labels: labels2,
+    datasets: [{
+        label: 'Total Pets',
+        data: <?php echo json_encode($count); ?>,
+        backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)',
+        'rgb(69, 18, 18)',
+        'rgb(73, 76, 99)',
+        'rgb(237, 12, 203)'
+
+        ],
+        hoverOffset: 4
+    }]
+    };
+
+    const config2 = {
+        type: 'pie',
+        data: data2,
+    };
+
+    const myChart2 = new Chart(
+        document.getElementById('myChart2'),
+        config2
+    );
+    </script>
+
     <script type="text/javascript">
        $(document).ready( function () {
             $('#datatable').DataTable({
