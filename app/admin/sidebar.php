649049582255d82de80b406f4428fbdb9bc4cdaa
n67h <night67hunter@gmail.com>
2023-05-09 04:39:02 +0800

    Add cdn for chartjs

--- app/admin/sidebar.php
+++ app/admin/sidebar.php
@@ -9,6 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <!-- custom css -->
     <link rel="stylesheet" href="../../resources/css/style.css">
+    <!-- cdn of chartjs -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
     <!-- start of main container -->
