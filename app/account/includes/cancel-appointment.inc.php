3c096a2ef8ee70b3e0dbf2e3f7df04d025ebd28f
n67h <night67hunter@gmail.com>
2023-05-09 17:23:38 +0800

    Change isset['post'] to cancel

--- app/account/includes/cancel-appointment.inc.php
+++ app/account/includes/cancel-appointment.inc.php
@@ -2,16 +2,16 @@
     session_start();
     require_once '../../includes/db.inc.php';
 
-    if(isset($_POST['delete'])){
+    if(isset($_POST['cancel'])){
         $delete_ua_appointment_id = mysqli_real_escape_string($conn, $_POST['delete_ua_appointment_id']);
 
         $sql = "UPDATE appointment SET status = 2 WHERE appointment_id = $delete_ua_appointment_id;";
         if(mysqli_query($conn, $sql)){
-            header("location: ../appointment.php");
+            header("location: ../appointment.php?status=waiting");
             die();
         }
 
     }else{
-        header("location: ../appointment.php");
+        header("location: ../appointment.php?status=waiting");
         die();
     }
